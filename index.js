9c9eed26ba064b60f6b02b3784546e46ba17cbe7
shuvojitsana <shuvojitsanabd2004@gmail.com>
2023-05-02 20:15:11 +0600

    added a recipes comnents

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000 ;
 
 const recipes = require('./data/recipes.json')
+
+app.use(cors());
 
 app.get('/', (req, res) =>{
     res.send('chef recipe hunter')
@@ -10,6 +13,12 @@ app.get('/', (req, res) =>{
 
 app.get('/recipes', (req, res) =>{
     res.send(recipes);
+})
+
+app.get('/recipes/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedRecipes = recipes.find(recipe => recipe._id === id);
+    res.send(selectedRecipes);
 })
 
 app.listen(port, () =>{
