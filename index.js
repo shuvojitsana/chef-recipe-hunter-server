01313d9c85b3d97f72ba378ae9965da067379c1c
shuvojitsana <shuvojitsanabd2004@gmail.com>
2023-05-02 16:14:37 +0600

    recipr.json file has commited now

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000 ;
 
+const recipes = require('./data/recipes.json')
+
 app.get('/', (req, res) =>{
     res.send('chef recipe hunter')
+})
+
+app.get('/recipes', (req, res) =>{
+    res.send(recipes);
 })
 
 app.listen(port, () =>{
